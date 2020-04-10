d56d279d0d5c9cd083e4f08f7d6ec26e5fcfb792
Max Wang <max@covar.com>
2020-04-10 15:55:17 -0400

    show the text sent from the code block

--- js/component/App.jsx
+++ js/component/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 export default function App(props) {
   console.log('Props from Jupyter Notebook:', props);
   return (
-    <h1>Test</h1>
+    <>
+      <h1>This is React.</h1>
+      <p>The message you sent was: {props.data.test}</p>
+    </>
   );
 }
