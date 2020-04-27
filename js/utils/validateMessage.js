2fd4e055a5c553e5ce57bd31eda4c5cb09f0210c
Max Wang <max@covar.com>
2020-04-27 10:14:39 -0400

    include all error messages in output

--- js/utils/validateMessage.js
+++ js/utils/validateMessage.js
@@ -2,7 +2,7 @@ import Ajv from 'ajv';
 import ymlFile from '../kgsSchema.yml';
 
 export default function validateMessage(message) {
-  const ajv = new Ajv();
+  const ajv = new Ajv({ allErrors: true });
   const valid = ajv.validate(ymlFile, message);
   return {
     valid,
